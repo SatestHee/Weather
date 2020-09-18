52139edff841467934f87aa1f6f3e8329023bcd3
Sandy1 <partdeux23@gmail.com>
2020-09-18 15:50:24 +0800

    toolTip move

--- src/DailyChart.js
+++ src/DailyChart.js
@@ -1,11 +1,11 @@
 import * as d3 from 'd3';
-import { bisect } from 'd3';
+import { text } from 'd3';
 import React, { useContext, useEffect, useRef } from 'react';
 import { DailyData } from './Forecast'
 
 function DailyChart() {
     const ref = useRef()
-    const lists = useContext(DailyData)
+    var lists = useContext(DailyData)
 
     var drawChart = () => {
         const svg = d3.select(ref.current)
@@ -20,10 +20,12 @@ function DailyChart() {
 
         const width = 600 - margin.left - margin.right;
         const height = 400 - margin.top - margin.bottom;
-
+        lists.forEach(day => {
+            day.dt = new Date(day.dt * 1000)
+        });
         //XAxis Date Format
         const x = d3.scaleTime()
-            .domain(d3.extent(lists, d => new Date(d.dt * 1000)))
+            .domain(d3.extent(lists, d => d.dt))
             .range([0, width])
         svg.select('#x')
             .attr("transform", "translate(0," + height + ")")
@@ -42,13 +44,13 @@ function DailyChart() {
             .attr("stroke", "steelblue")
             .attr("stroke-width", 3)
             .attr("d", d3.line()
-                .x(d => x(new Date(d.dt * 1000)))
+                .x(d => x(d.dt))
                 .y(d => y(d.temp))
             )
         var focus = d3.select("#focus")
 
         focus.select(".tooltip")
-            .attr("width", 100)
+            .attr("width", 300)
             .attr("height", 50)
             .attr("x", 10)
             .attr("y", -22)
@@ -70,13 +72,22 @@ function DailyChart() {
             .on("mouseover", () => focus.style("display", null))
             .on("mouseout", () => focus.style("display", "none"))
             .on("mousemove", (e, lists) => mousemove(e, lists))
-        var bisectDate = d3.bisector(function (d) { return new Date(d.dt * 1000) }).right;
+        var bisectDate = d3.bisector((d) => d.dt).right;
 
         function mousemove(e, lists) {
             var x0 = x.invert(e.pageX)
-            const test = bisectDate(lists, x0);
-            console.log('x0', x0)
-            console.log('lists', test)
+            const index = bisectDate(lists, x0);
+            const a = lists[index - 25].dt
+            console.log('a', a)
+            const b = lists[index - 25].temp
+            console.log('a', b)
+            // svg.select('#dot')
+            //     .attr('cx', x(a))
+            //     .attr('cy', y(b))
+            focus.attr("transform", `translate(${x(a)},${y(b)})`)
+            focus.select(".tooltip-date")
+                .text(d => 'SDate');
+
         }
 
 
